0a7a19fefc02a039410e8157f734372c1ee1100e
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-14 19:41:34 +0300

    js hw09 task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const elements = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  bodyBg: document.querySelector('body'),
+};
 
+let timerId = null;
+
+elements.btnStart.addEventListener('click', handlerClickStart);
+
+function handlerClickStart(event) {
+  timerId = setInterval(() => {
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    }
+      elements.bodyBg.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    event.target.disabled = true;
+};
+
+elements.btnStop.addEventListener('click', () => {
+    elements.bodyBg.style.backgroundColor = '#fafafa';
+    clearInterval(timerId);
+    elements.btnStart.disabled = false;
+});
