ee73b14dcc22bfa68a0c8a14573988901d1065c8
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-15 20:17:25 +0300

    js hw09 task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 const elements = {
     calendar: document.querySelector('#datetime-picker'),
@@ -9,13 +10,46 @@ const elements = {
     minutes: document.querySelector('[data-minutes]'),
     seconds: document.querySelector('[data-seconds')
 }
+let idTimerChoice = null;
+elements.btnStartTimer.disabled = true;
 
 const fp = flatpickr(elements.calendar, {
-    enableTime: true,
+  enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+    if (new Date(selectedDates).getTime() < new Date().getTime()) {
+      Report.failure(
+        'Wrong date or time for timer',
+        'You must choose a future date or time for timer',
+        'OK',
+      )}  
+  }
 })
+
+elements.calendar.addEventListener('input', () => {
+  return idTimerChoice = setInterval(() => {
+    new Date(fp.selectedDates).getTime() > new Date().getTime() ?
+      elements.btnStartTimer.disabled = false :
+      elements.btnStartTimer.disabled = true;
+  }, 1000)
+})
+
+elements.btnStartTimer.addEventListener('click', handlerClick)
+
+function handlerClick(event) {
+  clearInterval(idTimerChoice);
+  
+
+
+
+  elements.btnStartTimer.disabled = true;
+
+}
+
+
+
+
+console.log();
+
