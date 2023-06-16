3a71cc4176442572601ff29b0d0749a64e7e0662
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-16 23:53:52 +0300

    js hw09 task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,55 +1,91 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
 const elements = {
-    calendar: document.querySelector('#datetime-picker'),
-    btnStartTimer: document.querySelector('button[data-start]'),
-    days: document.querySelector('[data-days]'),
-    hours: document.querySelector('[data-hours]'),
-    minutes: document.querySelector('[data-minutes]'),
-    seconds: document.querySelector('[data-seconds')
-}
-let idTimerChoice = null;
-elements.btnStartTimer.disabled = true;
+  calendar: document.querySelector('#datetime-picker'),
+  btnStartTimer: document.querySelector('button[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds'),
+};
 
 const fp = flatpickr(elements.calendar, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  dateFormat: 'Y-m-d   H-i-S',
   onClose(selectedDates) {
-    if (new Date(selectedDates).getTime() < new Date().getTime()) {
+    if (new Date(selectedDates).getTime() < Date.now()) {
       Report.failure(
         'Wrong date or time for timer',
         'You must choose a future date or time for timer',
-        'OK',
-      )}  
+        'OK'
+      );
+    } else elements.btnStartTimer.disabled = false;
+  },
+});
+
+// let idTimerChoice = null;
+let idTimerPeriod = null;
+elements.btnStartTimer.disabled = true;
+
+// elements.calendar.addEventListener('input', () => {
+//   return (idTimerChoice = setInterval(() => {
+//     new Date(fp.selectedDates).getTime() > new Date().getTime()
+//       ? (elements.btnStartTimer.disabled = false)
+//       : (elements.btnStartTimer.disabled = true);
+//   }, 500));
+// });
+
+elements.btnStartTimer.addEventListener('click', () => {
+  // clearInterval(idTimerChoice);
+  if (new Date(fp.selectedDates).getTime() > Date.now()) {
+    elements.calendar.disabled = true;
+    elements.btnStartTimer.disabled = true;
+    idTimerPeriod = setInterval(updatePeriod, 1000);
+  } else
+    Report.failure(
+      'Wrong date or time for timer',
+      'You must choose a future date or time for timer',
+      'OK'
+    );
+});
+
+function updatePeriod() {
+  const periodOfTime = new Date(fp.selectedDates).getTime() - Date.now();
+  if (periodOfTime < 999) {
+    clearInterval(idTimerPeriod);
+    Report.success(
+      'CONGTATULATION',
+      'To run a new countdown timer select a date and/or time',
+      'OK'
+    );
+    elements.calendar.disabled = false;
   }
-})
-
-elements.calendar.addEventListener('input', () => {
-  return idTimerChoice = setInterval(() => {
-    new Date(fp.selectedDates).getTime() > new Date().getTime() ?
-      elements.btnStartTimer.disabled = false :
-      elements.btnStartTimer.disabled = true;
-  }, 1000)
-})
-
-elements.btnStartTimer.addEventListener('click', handlerClick)
-
-function handlerClick(event) {
-  clearInterval(idTimerChoice);
-  
-
-
-
-  elements.btnStartTimer.disabled = true;
-
+  const convertPeriod = convertMs(periodOfTime);
+  addLeadingZero(convertPeriod);
 }
 
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+  return { days, hours, minutes, seconds };
+}
 
-console.log();
-
+function addLeadingZero({ days, hours, minutes, seconds }) {
+  elements.days.textContent = String(days).padStart(2, '0');
+  elements.hours.textContent = String(hours).padStart(2, '0');
+  elements.minutes.textContent = String(minutes).padStart(2, '0');
+  elements.seconds.textContent = String(seconds).padStart(2, '0');
+}
