6bd85f58962d3e641b7f7f58513e99ea24e3b4ea
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-18 20:03:08 +0300

    update task 3 js-hw-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,29 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handlerForm);
+
+function handlerForm(event) {
+  event.preventDefault();
+  const { delay, step, amount } = form.elements;
+  
+  for (let i = 0; i < Number(amount.value); i += 1){
+    const promiseDelay = Number(delay.value) + Number(step.value) * i;
+    createPromise(i+1, promiseDelay).then()
+  }
+}
+
+
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+const shouldResolve = Math.random() > 0.3;
+if (shouldResolve) {
+  // Fulfill
   } else {
     // Reject
   }
 }
+
+console.dir(typeof (form));
 
 
