adb06924977b789530b7393f7b29a39489a464e9
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-06-19 08:47:48 +0300

    js hw09 task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', handlerForm);
@@ -8,22 +10,26 @@ function handlerForm(event) {
   
   for (let i = 0; i < Number(amount.value); i += 1){
     const promiseDelay = Number(delay.value) + Number(step.value) * i;
-    createPromise(i+1, promiseDelay).then()
-  }
-}
+    createPromise(i + 1, promiseDelay)
+      .then(({ position, promiseDelay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${promiseDelay}ms`)
+      })
+      .catch(({ position, promiseDelay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${promiseDelay}ms`)
+      });
+  };
+};
 
-
-
-
-function createPromise(position, delay) {
-const shouldResolve = Math.random() > 0.3;
-if (shouldResolve) {
-  // Fulfill
-  } else {
-    // Reject
-  }
-}
-
-console.dir(typeof (form));
-
-
+function createPromise(position, promiseDelay) {
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, promiseDelay });
+      } else {
+        reject({ position, promiseDelay });
+      }
+    }, promiseDelay);
+  })
+  return promise;
+};
